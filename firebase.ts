573e1ec7f97988731ad282160dc338c1b8e51125
minki3 <86992006+minki3@users.noreply.github.com>
2024-02-29 15:31:17 +0900

    Feat:FireBase설치

---
+++ firebase.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "님들꺼",
+  authDomain: "님들꺼",
+  projectId: "님들꺼",
+  storageBucket: "님들꺼",
+  messagingSenderId: "님들꺼",
+  appId: "님들꺼",
+  measurementId: "님들꺼",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
